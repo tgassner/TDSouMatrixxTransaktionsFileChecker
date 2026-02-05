51a156b2cc1ff61ff3957e6ab6bf6ca7f243412b
tgassner <thomas.gassner@gmx.net>
2026-02-05 17:16:20 +0100

    Projektdateien hinzufügen.

---
+++ SouMatrixxTransaktionsChecker/MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Transparentdesign.SouMatrixxTransaktionsChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            buttonFindSuspiciousTransactions = new Button();
+            textBoxTransactionDirectory = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            listViewOldFiles = new ListView();
+            buttonFindTransactionDirectory = new Button();
+            textBoxLog = new TextBox();
+            label1 = new Label();
+            labelOlderNullFilledFiles = new Label();
+            listViewOldFilesFilledWithNulls = new ListView();
+            labelLog = new Label();
+            labelTransactionFilesRoot = new Label();
+            toolTipChooseDirectory = new ToolTip(components);
+            buttonDeleteSelectedOldFiles = new Button();
+            buttonDeleteSelectedOldNULLFiles = new Button();
+            SuspendLayout();
+            // 
+            // buttonFindSuspiciousTransactions
+            // 
+            buttonFindSuspiciousTransactions.Location = new Point(12, 41);
+            buttonFindSuspiciousTransactions.Name = "buttonFindSuspiciousTransactions";
+            buttonFindSuspiciousTransactions.Size = new Size(386, 39);
+            buttonFindSuspiciousTransactions.TabIndex = 0;
+            buttonFindSuspiciousTransactions.Text = "### Find Suspicious Transactions ###";
+            buttonFindSuspiciousTransactions.UseVisualStyleBackColor = true;
+            buttonFindSuspiciousTransactions.Click += buttonFindSuspiciousTransactions_Click;
+            // 
+            // textBoxTransactionDirectory
+            // 
+            textBoxTransactionDirectory.Location = new Point(143, 12);
+            textBoxTransactionDirectory.Name = "textBoxTransactionDirectory";
+            textBoxTransactionDirectory.Size = new Size(491, 23);
+            textBoxTransactionDirectory.TabIndex = 1;
+            textBoxTransactionDirectory.TextChanged += textBoxTransactionDirectory_TextChanged;
+            // 
+            // listViewOldFiles
+            // 
+            listViewOldFiles.Location = new Point(12, 101);
+            listViewOldFiles.Name = "listViewOldFiles";
+            listViewOldFiles.Size = new Size(1240, 202);
+            listViewOldFiles.TabIndex = 2;
+            listViewOldFiles.UseCompatibleStateImageBehavior = false;
+            listViewOldFiles.View = View.Details;
+            listViewOldFiles.SelectedIndexChanged += listViewOldFiles_SelectedIndexChanged;
+            // 
+            // buttonFindTransactionDirectory
+            // 
+            buttonFindTransactionDirectory.Location = new Point(640, 12);
+            buttonFindTransactionDirectory.Name = "buttonFindTransactionDirectory";
+            buttonFindTransactionDirectory.Size = new Size(25, 23);
+            buttonFindTransactionDirectory.TabIndex = 3;
+            buttonFindTransactionDirectory.Text = "…";
+            toolTipChooseDirectory.SetToolTip(buttonFindTransactionDirectory, "choose Directory…");
+            buttonFindTransactionDirectory.UseVisualStyleBackColor = true;
+            buttonFindTransactionDirectory.Click += buttonFindTransactionDirectory_Click;
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.Location = new Point(12, 563);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.ReadOnly = true;
+            textBoxLog.Size = new Size(1240, 186);
+            textBoxLog.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Older Files:";
+            // 
+            // labelOlderNullFilledFiles
+            // 
+            labelOlderNullFilledFiles.AutoSize = true;
+            labelOlderNullFilledFiles.Location = new Point(12, 346);
+            labelOlderNullFilledFiles.Name = "labelOlderNullFilledFiles";
+            labelOlderNullFilledFiles.Size = new Size(177, 15);
+            labelOlderNullFilledFiles.TabIndex = 6;
+            labelOlderNullFilledFiles.Text = "Older NULL  filled or empty Files";
+            // 
+            // listViewOldFilesFilledWithNulls
+            // 
+            listViewOldFilesFilledWithNulls.Location = new Point(12, 364);
+            listViewOldFilesFilledWithNulls.Name = "listViewOldFilesFilledWithNulls";
+            listViewOldFilesFilledWithNulls.Size = new Size(1240, 140);
+            listViewOldFilesFilledWithNulls.TabIndex = 7;
+            listViewOldFilesFilledWithNulls.UseCompatibleStateImageBehavior = false;
+            listViewOldFilesFilledWithNulls.View = View.Details;
+            listViewOldFilesFilledWithNulls.SelectedIndexChanged += listViewOldFilesFilledWithNulls_SelectedIndexChanged;
+            // 
+            // labelLog
+            // 
+            labelLog.AutoSize = true;
+            labelLog.Location = new Point(12, 545);
+            labelLog.Name = "labelLog";
+            labelLog.Size = new Size(30, 15);
+            labelLog.TabIndex = 8;
+            labelLog.Text = "Log:";
+            // 
+            // labelTransactionFilesRoot
+            // 
+            labelTransactionFilesRoot.AutoSize = true;
+            labelTransactionFilesRoot.Location = new Point(12, 15);
+            labelTransactionFilesRoot.Name = "labelTransactionFilesRoot";
+            labelTransactionFilesRoot.Size = new Size(125, 15);
+            labelTransactionFilesRoot.TabIndex = 9;
+            labelTransactionFilesRoot.Text = "Transaction Files Root:";
+            // 
+            // buttonDeleteSelectedOldFiles
+            // 
+            buttonDeleteSelectedOldFiles.Location = new Point(12, 309);
+            buttonDeleteSelectedOldFiles.Name = "buttonDeleteSelectedOldFiles";
+            buttonDeleteSelectedOldFiles.Size = new Size(250, 23);
+            buttonDeleteSelectedOldFiles.TabIndex = 10;
+            buttonDeleteSelectedOldFiles.Text = "DELETE selected Old Files";
+            buttonDeleteSelectedOldFiles.UseVisualStyleBackColor = true;
+            buttonDeleteSelectedOldFiles.EnabledChanged += buttonDeleteSelectedOldFiles_EnabledChanged;
+            buttonDeleteSelectedOldFiles.Click += buttonDeleteSelectedOldFiles_Click;
+            // 
+            // buttonDeleteSelectedOldNULLFiles
+            // 
+            buttonDeleteSelectedOldNULLFiles.Location = new Point(12, 510);
+            buttonDeleteSelectedOldNULLFiles.Name = "buttonDeleteSelectedOldNULLFiles";
+            buttonDeleteSelectedOldNULLFiles.Size = new Size(250, 23);
+            buttonDeleteSelectedOldNULLFiles.TabIndex = 11;
+            buttonDeleteSelectedOldNULLFiles.Text = "DELETE seleted Old NULL Files";
+            buttonDeleteSelectedOldNULLFiles.UseVisualStyleBackColor = true;
+            buttonDeleteSelectedOldNULLFiles.EnabledChanged += buttonDeleteSelectedOldNULLFiles_EnabledChanged;
+            buttonDeleteSelectedOldNULLFiles.Click += buttonDeleteSelectedOldNULLFiles_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 761);
+            Controls.Add(buttonDeleteSelectedOldNULLFiles);
+            Controls.Add(buttonDeleteSelectedOldFiles);
+            Controls.Add(labelTransactionFilesRoot);
+            Controls.Add(labelLog);
+            Controls.Add(listViewOldFilesFilledWithNulls);
+            Controls.Add(labelOlderNullFilledFiles);
+            Controls.Add(label1);
+            Controls.Add(textBoxLog);
+            Controls.Add(buttonFindTransactionDirectory);
+            Controls.Add(listViewOldFiles);
+            Controls.Add(textBoxTransactionDirectory);
+            Controls.Add(buttonFindSuspiciousTransactions);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Sou.Matrixx Transaction Checker";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonFindSuspiciousTransactions;
+        private TextBox textBoxTransactionDirectory;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ListView listViewOldFiles;
+        private Button buttonFindTransactionDirectory;
+        private TextBox textBoxLog;
+        private Label label1;
+        private Label labelOlderNullFilledFiles;
+        private ListView listViewOldFilesFilledWithNulls;
+        private Label labelLog;
+        private Label labelTransactionFilesRoot;
+        private ToolTip toolTipChooseDirectory;
+        private Button buttonDeleteSelectedOldFiles;
+        private Button buttonDeleteSelectedOldNULLFiles;
+    }
+}
---
+++ SouMatrixxTransaktionsChecker/MainForm.cs
@@ -0,0 +1,304 @@
+using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
+using System.Security;
+
+namespace Transparentdesign.SouMatrixxTransaktionsChecker
+{
+    public partial class MainForm : Form
+    {
+        private readonly Color lightGreen = Color.FromArgb(255, 150, 220, 150);
+        private readonly Color lightRed = Color.FromArgb(255, 240, 150, 150);
+        private readonly Color lightYellow = Color.FromArgb(255, 255, 255, 180);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFindTransactionDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Multiselect = false;
+            if (!String.IsNullOrWhiteSpace(textBoxTransactionDirectory.Text) && new DirectoryInfo(textBoxTransactionDirectory.Text).Exists)
+            {
+                dialog.SelectedPath = textBoxTransactionDirectory.Text;
+            }
+
+            DialogResult dialogResult = dialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                string transactionDirectory = textBoxTransactionDirectory.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void textBoxTransactionDirectory_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string path = textBox.Text;
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                textBox.BackColor = this.lightYellow;
+                buttonFindSuspiciousTransactions.Enabled = false;
+                return;
+            }
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            Color bkColor = (directoryInfo.Exists ? this.lightGreen : this.lightRed);
+            buttonFindSuspiciousTransactions.Enabled = directoryInfo.Exists;
+            textBox.BackColor = bkColor;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            buttonFindSuspiciousTransactions.Enabled = false;
+            buttonDeleteSelectedOldFiles.Enabled = false;
+            buttonDeleteSelectedOldNULLFiles.Enabled = false;
+
+            string transactionDirectory = Properties.Settings.Default.transactionDirectory;
+            this.textBoxTransactionDirectory.Text = transactionDirectory;
+
+            listViewOldFiles.Columns.Clear();
+            listViewOldFiles.Columns.Add("Filename", 250);
+            listViewOldFiles.Columns.Add("Filesize", 50);
+            listViewOldFiles.Columns.Add("Age [Days]", 80);
+            listViewOldFiles.Columns.Add("Creation Date", 150);
+            listViewOldFiles.Columns.Add("Path", 350);
+
+            listViewOldFilesFilledWithNulls.Columns.Clear();
+            listViewOldFilesFilledWithNulls.Columns.Add("Filename", 250);
+            listViewOldFilesFilledWithNulls.Columns.Add("Filesize", 50);
+            listViewOldFilesFilledWithNulls.Columns.Add("Age [Days]", 80);
+            listViewOldFilesFilledWithNulls.Columns.Add("Creation Date", 150);
+            listViewOldFilesFilledWithNulls.Columns.Add("Path", 350);
+        }
+
+        private void buttonFindSuspiciousTransactions_Click(object sender, EventArgs e)
+        {
+            this.listViewOldFiles.Items.Clear();
+            this.listViewOldFilesFilledWithNulls.Items.Clear();
+            string transactionDirectory = textBoxTransactionDirectory.Text;
+            if (String.IsNullOrWhiteSpace(transactionDirectory) || !new DirectoryInfo(transactionDirectory).Exists)
+            {
+                MessageBox.Show("The path \"" + transactionDirectory + "\" is not valid!");
+                return;
+            }
+            Properties.Settings.Default.transactionDirectory = transactionDirectory;
+            Properties.Settings.Default.Save();
+
+            try
+            {
+                traverseTreeForEach(transactionDirectory, (fullPathFilename) =>
+                {
+                    try
+                    {
+                        FileInfo fileInfo = new FileInfo(fullPathFilename);
+                        DateTime creationTime = fileInfo.CreationTime;
+                        int ageInHours = (int)DateTime.Now.Subtract(creationTime).TotalHours;
+                        int ageInDays = (int)DateTime.Now.Subtract(creationTime).TotalDays;
+
+                        if (ageInHours > 50)
+                        {
+                            ListViewItem itemOldFile = new ListViewItem(new string[] {
+                                fileInfo.Name,
+                                fileInfo.Length.ToString(),
+                                ageInDays.ToString(),
+                                fileInfo.CreationTime.ToShortDateString(),
+                                fileInfo.DirectoryName ?? ""});
+                            itemOldFile.Tag = fileInfo.FullName;
+                            listViewOldFiles.Items.Add(itemOldFile);
+
+                            // Do nothing with the data except read it.
+                            byte[] data = File.ReadAllBytes(fullPathFilename);
+                            bool otherThanNull = false;
+                            foreach (byte b in data)
+                            {
+                                if (b != 0)
+                                {
+                                    otherThanNull = true;
+                                    break;
+                                }
+                            }
+                            if (!otherThanNull)
+                            {
+                                ListViewItem itemNullFile = new ListViewItem(new string[] {
+                                    fileInfo.Name,
+                                    fileInfo.Length.ToString(),
+                                    ageInDays.ToString(),
+                                    fileInfo.CreationTime.ToShortDateString(),
+                                    fileInfo.DirectoryName ?? ""});
+                                itemNullFile.Tag = fileInfo.FullName;
+                                this.listViewOldFilesFilledWithNulls.Items.Add(itemNullFile);
+                            }
+                        }
+                    }
+                    catch (FileNotFoundException) { }
+                    catch (IOException) { }
+                    catch (UnauthorizedAccessException) { }
+                    catch (SecurityException) { }
+
+                    //logToTextBox(fullPathFilename);
+                });
+            }
+            catch (ArgumentException)
+            {
+                logToTextBox(@"The directory 'C:\Program Files' does not exist.");
+            }
+        }
+
+        private void logToTextBox(string t)
+        {
+            textBoxLog.Text = t + "\r\n" + textBoxLog.Text;
+        }
+
+        public void traverseTreeForEach(string root, Action<string> action)
+        {
+            //Count of files traversed and timer for diagnostic output
+            int fileCount = 0;
+            var sw = Stopwatch.StartNew();
+
+            // Data structure to hold names of subfolders to be examined for files.
+            Stack<string> dirs = new Stack<string>();
+
+            if (!Directory.Exists(root))
+            {
+                throw new ArgumentException(
+                    "The given root directory doesn't exist.", nameof(root));
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs = { };
+                string[] files = { };
+
+                try
+                {
+                    subDirs = Directory.GetDirectories(currentDir);
+                }
+                // Thrown if we do not have discovery permission on the directory.
+                catch (UnauthorizedAccessException e)
+                {
+                    logToTextBox(e.Message);
+                    continue;
+                }
+                // Thrown if another process has deleted the directory after we retrieved its name.
+                catch (DirectoryNotFoundException e)
+                {
+                    logToTextBox(e.Message);
+                    continue;
+                }
+
+                try
+                {
+                    files = Directory.GetFiles(currentDir);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    logToTextBox(e.Message);
+                    continue;
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    logToTextBox(e.Message);
+                    continue;
+                }
+                catch (IOException e)
+                {
+                    logToTextBox(e.Message);
+                    continue;
+                }
+
+                // Execute in parallel if there are enough files in the directory.
+                // Otherwise, execute sequentially.Files are opened and processed
+                // synchronously but this could be modified to perform async I/O.
+                try
+                {
+                    foreach (var file in files)
+                    {
+                        action(file);
+                        fileCount++;
+                    }
+                }
+                catch (AggregateException ae)
+                {
+                    ae.Handle((ex) =>
+                    {
+                        if (ex is UnauthorizedAccessException)
+                        {
+                            // Here we just output a message and go on.
+                            logToTextBox(ex.Message);
+                            return true;
+                        }
+                        // Handle other exceptions here if necessary...
+
+                        return false;
+                    });
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+
+            // For diagnostic purposes.
+            logToTextBox($"Processed {fileCount} files in {sw.ElapsedMilliseconds} milliseconds");
+        }
+
+        private void listViewOldFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonDeleteSelectedOldFiles.Enabled = listViewOldFiles.SelectedItems.Count > 0;
+        }
+
+        private void listViewOldFilesFilledWithNulls_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonDeleteSelectedOldNULLFiles.Enabled = listViewOldFilesFilledWithNulls.SelectedItems.Count > 0;
+        }
+
+        private void buttonDeleteSelectedOldFiles_EnabledChanged(object sender, EventArgs e)
+        {
+            buttonDeleteSelectedOldFiles.BackColor = buttonDeleteSelectedOldFiles.Enabled
+                ? Color.IndianRed
+                : SystemColors.Control;
+
+            buttonDeleteSelectedOldFiles.ForeColor = buttonDeleteSelectedOldFiles.Enabled
+                ? Color.White
+                : SystemColors.ControlText;
+        }
+
+        private void buttonDeleteSelectedOldNULLFiles_EnabledChanged(object sender, EventArgs e)
+        {
+            buttonDeleteSelectedOldNULLFiles.BackColor = buttonDeleteSelectedOldNULLFiles.Enabled
+                ? Color.IndianRed
+                : SystemColors.Control;
+
+            buttonDeleteSelectedOldNULLFiles.ForeColor = buttonDeleteSelectedOldNULLFiles.Enabled
+                ? Color.White
+                : SystemColors.ControlText;
+        }
+
+        private void buttonDeleteSelectedOldFiles_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listViewOldFiles.SelectedItems)
+            {
+                logToTextBox(item.ToString());
+                if (item != null && item.Tag != null && new FileInfo(item.Tag.ToString()).Exists)
+                {
+                    logToTextBox(item.Tag.ToString());
+                }
+            }
+        }
+
+        private void buttonDeleteSelectedOldNULLFiles_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listViewOldFilesFilledWithNulls.SelectedItems)
+            {
+                if (item != null && item.Tag != null && new FileInfo(item.Tag.ToString()).Exists)
+                {
+                    logToTextBox(item.Tag.ToString());
+                }
+            }
+        }
+    }
+}
